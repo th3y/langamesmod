65e723042186e5b65a461a56340de1a3f7e4d219
JNC <th3y@hotmail.com>
2018-09-17 01:01:33 -0500

    Add files via upload

--- mj/js/jquery.prettyPhoto.js
+++ mj/js/jquery.prettyPhoto.js
@@ -31,7 +31,6 @@
             <a href="#" class="pp_arrow_next">Next</a> \
            </div> \
            <p class="pp_description"></p> \
-           <div class="pp_social">{pp_social}</div> \
            <a class="pp_close" href="#">Close</a> \
           </div> \
          </div> \
